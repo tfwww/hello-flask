9441f25aef4954bc37d5ba99bc1135d914d52fba
wmzhong <zhongwm.net@gmail.com>
2015-11-21 20:36:21 +0800

    3e Add the time moment.

--- app/hello.py
+++ app/hello.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 
 bootstrap = Bootstrap(app)
 manager = Manager(app)
+moment = Moment(app)
 
-@app.route('/index')
+@app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
+
 
 @app.route('/user/<name>')
 def user(name):
