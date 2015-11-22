6c94b06acf149f60c944fe7b2401013d7102b8af
wmzhong <zhongwm.net@gmail.com>
2015-11-22 19:46:18 +0800

    4b redirect and session

--- app/hello.py
+++ app/hello.py
@@ -1,19 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
-from datetime import datetime
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 bootstrap = Bootstrap(app)
 manager = Manager(app)
 moment = Moment(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	name = None
+	form = NameForm()
+	if form.validate_on_submit():
+		session['name'] = form.name.data
+		return redirect(url_for('index'))
+	return render_template('index.html', form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
